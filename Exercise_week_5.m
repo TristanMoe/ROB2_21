c97a41dad9a8d59ee0145a803d18be484784ffe3
Martin <martinx1995x@hotmail.com>
2020-03-04 11:15:54 +0100

    code chagne for push

--- Exercise_week_5.m
+++ Exercise_week_5.m
@@ -18,33 +18,33 @@ velmsg = rosmessage(robot);
 %% Get robots start position
 odomdata = receive(odom, 2); 
 pose = odomdata.Pose.Pose;
-start = [pose.Position.X, pose.Position.Y]; 
+start = [round(pose.Position.X)+1, round(pose.Position.Y)+1]; 
 
-goal = [5,5];
+goal = [round(pose.Position.X)+10,round(pose.Position.Y)+10];
 dx = DXform(map);
 dx.plan(goal); 
 path = dx.query(start, 'animate'); 
-path = path(2:2:end,:); 
+%path = path(2:2:end,:); 
 
 %%
 % Set controller
 controller = robotics.PurePursuit;
 controller.Waypoints = path; 
-controller.DesiredLinearVelocity = 0.3; 
-controller.MaxAngularVelocity = 0.6; 
-controller.LookaheadDistance = 0.5;
+controller.DesiredLinearVelocity = 0.2; 
+controller.MaxAngularVelocity = 2; 
+controller.LookaheadDistance = 0.2;
 
 % Set movement properties
-goalRadius = 0.1; 
+goalRadius = 0.5; 
 distanceToGoal = norm(start - goalRadius); 
 
 % Robot control loop
-while( distanceToGoal > goalRadius )
+while(distanceToGoal > goalRadius)
     % Get turtlebot pose 
     odomdata = receive(odom, 2); 
     pose = odomdata.Pose.Pose;
     theta = pose.Orientation.W;
-    poseVector = [pose.Position.X, pose.Position.Y, theta]; 
+    poseVector = [round(pose.Position.X+1) round(pose.Position.Y+1) 0]; 
     
     % Get new values for robot
     [linVel, angVel, targetDir] = controller(poseVector);   
@@ -52,5 +52,5 @@ while( distanceToGoal > goalRadius )
     % Pass new information to turtlebot. 
     velmsg.Linear.X = linVel; 
     velmsg.Angular.Z = angVel;       
-    send(robot, velmsg);    
+    send(robot, velmsg);        
 end
