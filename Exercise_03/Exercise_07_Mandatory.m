8ad73594cefb995daf056036e1f32eabf5f3a64b
TristanMoe <Trilleo@hotmail.com>
2020-03-21 11:14:41 +0100

    I FUCKING HATE ROB2

--- Exercise_03/Exercise_07_Mandatory.m
+++ Exercise_03/Exercise_07_Mandatory.m
@@ -1,16 +1,16 @@
-%% Initiate 
-rosshutdown
-
-% !!! REMEMBER TO CHANGE IP BASED ON HOST !!!
-setenv('ROS_MASTER_URI','http://192.168.87.107:11311')
-setenv('ROS_IP','192.168.87.106')
-rosinit('http://192.168.87.107:11311','NodeHost','192.168.87.106');
-
-%% Subscribe to Laser
-laser = rossubscriber('/scan');
-% Load scan, timeout 3 seconds
-scan = receive(laser,3);
-plot(scan)
+% %% Initiate 
+% rosshutdown
+% 
+% % !!! REMEMBER TO CHANGE IP BASED ON HOST !!!
+% setenv('ROS_MASTER_URI','http://192.168.87.107:11311')
+% setenv('ROS_IP','192.168.87.106')
+% rosinit('http://192.168.87.107:11311','NodeHost','192.168.87.106');
+% 
+% %% Subscribe to Laser
+% laser = rossubscriber('/scan');
+% % Load scan, timeout 3 seconds
+% scan = receive(laser,3);
+% plot(scan)
 
 %% Simulation
 exampleHelperROSLoadMessages
@@ -33,14 +33,9 @@ axis equal
 figure(); 
 plot(scan);
 
-%% Resize grid & data 
-mat = scale(cart); 
-imshow(mat);
-getObstacleInformation(mat); 
-%% Hough Transform & Get lines
+%% Hough Transform & get lines based on peaks. 
+% Find overall shortest distance for all lines.
 lines = getObstacleInformation(cart);
-
-%% Find overall shortest distance for all lines.
 
 for k = 1:length(lines)
     line = lines(k); 
@@ -69,12 +64,6 @@ for k = 1:length(lines)
     P2 = [lines(k).point2];
     distP2 = pdist([X;P2], 'euclidean');
     
-    % Find linear function for P1, P2
-    coeff = polyfit(P1, P2, 1);   
-    a = coeff(1);
-    b = coeff(2);
-    y_pos_rho = a*x_rho+b; 
-    
     % Check if x_rho, y_rho is with P1 & P2
     % Find largest x-value
     P_points = [P1(1) P2(1)];
@@ -86,46 +75,21 @@ for k = 1:length(lines)
         distLineShortest(k).angle = lines(k).theta;  
     else
         X = [0,0];
-        minDist = min([distP1, distP2]); 
-        distLineShortest(k).dist = minDist;              
-        distLineShortest(k).angle = atan2(P1(1), P1(2));  
+        P_dist = [distP1; distP2]; 
+        P_minPoints = [P1; P2];
+        
+        [minDist, indexMin] = min(P_dist); 
+        distLineShortest(k).dist = minDist;    
+        
+        P_min = P_minPoints(indexMin,:);         
+        distLineShortest(k).angle = atan2d(P_min(1), P_min(2)); 
     end
 end
 
 [distShortest, index] = min([distLineShortest(:).dist]);
 angleShortest = distLineShortest(index).angle;
 
-
-%% Find range between two points
-X = [0,0;0.5781,1.288];
-dist = pdist(X, 'euclidean'); 
-
-%% Function to extract range-angle coordinate
-% Robot range to wall 
-% Angle between robot heading & wall 
-
-function [mat] = scale(cart)
-    gridSize = 100; 
-    x = cart(:,2); % x-position
-    d = cart(:,1); % depth
-
-    xoffset = min(x); 
-    xscale = (max(x)-min(x)); 
-
-    xi = (x - xoffset)/xscale; % normalize data 0-1;
-    xi = round(xi*(gridSize-1))+1; 
-
-    doffset = min(d); 
-    dscale = (max(d)-min(d));
-
-    di = (d - doffset)/dscale; 
-    di = round(di*(gridSize-1))+1; 
-    ind = sub2ind([gridSize gridSize], xi, di); 
-
-    mat = zeros(gridSize); 
-    mat(ind) = 1; 
-end
-
+%%
 % Function to get distance to wall
 % Robot angle between wall and x-axis 
 function lines = getObstacleInformation(cart)
@@ -140,13 +104,18 @@ hPeaks = houghpeaks(H, numpeaks, 'Threshold', minPeakThreshold);
 lines = houghlines(cart,theta,rho,hPeaks,'FillGap',5,'MinLength',minLineThreshold);
 
 % Show lines
-imshow(cart), hold on,
-for k = 1:length(lines)
-   xy = [lines(k).point1; lines(k).point2];
-   plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
-end
+%imshow(cart), hold on,
+%for k = 1:length(lines)
+%   xy = [lines(k).point1; lines(k).point2];
+%   plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+%end
 end
 
-
+%% TEST: Find range between two points
+%X = [0,0];
+%Y = [0.6237, 1.185];
+%XY = [X;Y];
+%angle = atan2d(Y(1), Y(2)); 
+%dist = pdist(XY, 'euclidean'); 
 
 
